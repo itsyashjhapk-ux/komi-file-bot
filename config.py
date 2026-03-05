af197949d2f15f7450acc8be4481ad96637a9a88
itsyashjhapk-ux <itsyashjhapk@gmail.com>
2026-03-05 19:24:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 API_ID = 21592881
 API_HASH = eba06a0d465f1d1797f0e92e97ac68ad
 BOT_TOKEN = 8240874907:AAEvesmr0JbkAAynmLLQtQBR8EOVzqMXTEM
+BOT_USERNAME = "Komi_link_Provider_bot"
+START_PIC = https://i.ibb.co/YTLtj7r2/d43eb18b6542.jpg
